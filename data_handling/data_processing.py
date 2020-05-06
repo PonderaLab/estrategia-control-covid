be983160f65a88184ad0e68e5e7ce763adfabb35
Blas Kolic <blas.kolic@ouce.ox.ac.uk>
2020-05-06 15:53:35 +0100

    Actualicé scripts para lectura y procesamiento de datos.

--- data_handling/data_processing.py
+++ data_handling/data_processing.py
@@ -1,29 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import pandas as pd
 
-### DATOS POR ESTADO ###
-def series_panel_por_estado(datos_abiertos, entidades):
+## Función principal de procesamiento de datos abiertos
+def series_panel_por_estado(datos_abiertos):
     """
     Genera una tabla de panel con los siguientes datos, donde cada panel es un estado:
         - pruebas_diarias
         - confirmados_diarios
-        - sospechosos_diarios
-        - negativos_diarios
         - hospitalizados_diarios
-        - uci_diarios
-        - ambulatorios diarios
         - fallecidos_diarios
         - pruebas_acumuladas
         - confirmados_acumulados
-        - sospechosos_acumulados
-        - negativos_acumulados
-        - uci_acumulados
-        - ambulatorios_acumulados
+        - hospitalizados_acumulados
         - fallecidos_acumulados
 
-    Inputs:
-    - datos_abiertos: datos_abiertos abiertos de COVID-19 en México disponibles en [1].
-    - entidades: diccionario {nombre_entidad: clave_entidad}
+    Input:
+    - datos_abiertos: datos abiertos de COVID-19 en México disponibles en [1].
 
     Output:
     - series: Series de tiempo para cada estado para cada columna mencionada arriba.
@@ -41,27 +36,11 @@ def series_panel_por_estado(datos_abiertos, entidades):
               .count()['ORIGEN'])
 
     confirmados = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 1) ]
-              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])                   
-              .count()['ORIGEN'])
-
-    sospechosos = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 3) ]
               .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
-              .count()['ORIGEN'])
-
-    negativos = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 2) ]
-              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])                 
               .count()['ORIGEN'])
 
     # incluyendo uci
     hospitalizados = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 1) & (datos_abiertos['TIPO_PACIENTE'] == 2) ]
-              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
-              .count()['ORIGEN'])
-
-    uci = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 1) & (datos_abiertos['UCI'] == 1) ]
-              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
-              .count()['ORIGEN'])
-
-    ambulatorios = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 1) & (datos_abiertos['TIPO_PACIENTE'] == 1) ]
               .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
 
@@ -70,15 +49,10 @@ def series_panel_por_estado(datos_abiertos, entidades):
               .count()['ORIGEN'])
     # Convierte las fechas de defunción de str a fecha
     fallecidos.index.set_levels( pd.to_datetime(fallecidos.index.levels[1]), level=1, inplace=True )
-#     fallecidos.index = pd.to_datetime(fallecidos.index)
 
     df.loc[:,'pruebas_diarias']         = pruebas
     df.loc[:,'confirmados_diarios']     = confirmados
-    df.loc[:,'sospechosos_diarios']     = sospechosos
-    df.loc[:,'negativos_diarios']       = negativos
     df.loc[:,'hospitalizados_diarios']  = hospitalizados
-    df.loc[:,'uci_diarios']             = uci
-    df.loc[:,'ambulatorios_diarios']    = ambulatorios
     df.loc[:,'fallecidos_diarios']      = fallecidos
 
     # Convierte str a fechas y llena hoyos de fechas con ceros
@@ -87,22 +61,24 @@ def series_panel_por_estado(datos_abiertos, entidades):
     df.index.set_levels( idx, level=1, inplace=True )
     df.index.levels[1].name = 'Fecha'
     df = df.fillna(0)
-        
-    # Cambia claves de entidades federativas por sus respectivos nombres  
-    df.index.set_levels( entidades.keys, level=0, inplace=True )
-    
+
+    # Lista de entidades federativas con nombres oficiales
+    entidades = get_lista_entidades()
+    df.index.set_levels( entidades, level=0, inplace=True )
+
     # Genera series acumuladas para cada tipo de caso
     df.loc[:,'pruebas_acumuladas'] = df.loc[:,'pruebas_diarias'].groupby(level=0).cumsum()
     df.loc[:,'confirmados_acumulados'] = df.loc[:,'confirmados_diarios'].groupby(level=0).cumsum()
-    df.loc[:,'sospechosos_acumulados'] = df.loc[:,'sospechosos_diarios'].groupby(level=0).cumsum()
-    df.loc[:,'negativos_acumulados'] = df.loc[:,'negativos_diarios'].groupby(level=0).cumsum()
     df.loc[:,'hospitalizados_acumulados'] = df.loc[:,'hospitalizados_diarios'].groupby(level=0).cumsum()
-    df.loc[:,'uci_acumulados'] = df.loc[:,'uci_diarios'].groupby(level=0).cumsum()
-    df.loc[:,'ambulatorios_acumulados'] = df.loc[:,'ambulatorios_diarios'].groupby(level=0).cumsum()
     df.loc[:,'fallecidos_acumulados']   = df.loc[:,'fallecidos_diarios'].groupby(level=0).cumsum()
 
     return df.astype('int')
 
-## HELPER FUNCTIONS 
-def get_serie_nacional(series_panel): return series.groupby(level=1).sum()
-def get_serie_estatal(series_panel, estado): return series.loc[estado]
+## Funciones útiles
+def get_serie_nacional(series): return series.groupby(level=1).sum()
+def get_serie_estatal(series, estado): return series.loc[estado]
+
+## Funciones de ayuda
+def get_lista_entidades(path_catalogos='./data/diccionario_datos_covid19/Catalogos_0412.xlsx'):
+    return pd.read_excel(path_catalogos,
+              sheet_name='Catálogo de ENTIDADES')['ENTIDAD_FEDERATIVA'].values
