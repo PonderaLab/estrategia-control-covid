37181577f5eb995d8e076042cfac1675751ba6c0
Blas Kolic <blas.kolic@ouce.ox.ac.uk>
2020-05-06 13:37:26 +0100

    Crea tabla de panel con cada estado en lugar de una tabla individual.

--- data_handling/data_processing.py
+++ data_handling/data_processing.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 
 ### DATOS POR ESTADO ###
-def dataframe_estado(datos, entidades, estado='QUERÉTARO'):
+def series_panel_por_estado(datos_abiertos, entidades):
     """
-    Genera una tabla con los siguientes datos a nivel `estado`*:
+    Genera una tabla de panel con los siguientes datos, donde cada panel es un estado:
         - pruebas_diarias
         - confirmados_diarios
         - sospechosos_diarios
@@ -22,62 +22,55 @@ def dataframe_estado(datos, entidades, estado='QUERÉTARO'):
         - fallecidos_acumulados
 
     Inputs:
-    - datos: datos abiertos de COVID-19 en México disponibles en [1].
+    - datos_abiertos: datos_abiertos abiertos de COVID-19 en México disponibles en [1].
     - entidades: diccionario {nombre_entidad: clave_entidad}
-    - estado: Default='NACIONAL'. Nombre del estado para realizar la tabla
 
     Output:
-    - series: Series de tiempo para cada una de las columnas mencionadas arriba.
+    - series: Series de tiempo para cada estado para cada columna mencionada arriba.
 
-    [1]: https://www.gob.mx/salud/documentos/datos-abiertos-152127
-
-    *Si `estado = 'NACIONAL'`, genera la tabla agregada de todo México
+    [1]: https://www.gob.mx/salud/documentos/datos_abiertos-abiertos-152127
     """
-
 
     df = pd.DataFrame()
 
+    datos_abiertos['FECHA_INGRESO'] = pd.to_datetime(datos_abiertos['FECHA_INGRESO'])
+    datos_abiertos['FECHA_SINTOMAS'] = pd.to_datetime(datos_abiertos['FECHA_SINTOMAS'])
 
-    if estado != 'NACIONAL':
-        # Toma datos del estado únicamente
-        cve_entidad = entidades[estado]
-        datos = datos[ datos['ENTIDAD_UM'] == cve_entidad ]
-
-    datos['FECHA_INGRESO'] = pd.to_datetime(datos['FECHA_INGRESO'])
-
-    pruebas = (datos
-              .groupby('FECHA_INGRESO')
+    pruebas = (datos_abiertos
+              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
 
-    confirmados = (datos[ (datos['RESULTADO'] == 1) ]
-              .groupby('FECHA_INGRESO')
+    confirmados = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 1) ]
+              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])                   
               .count()['ORIGEN'])
 
-    sospechosos = (datos[ (datos['RESULTADO'] == 3) ]
-              .groupby('FECHA_INGRESO')
+    sospechosos = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 3) ]
+              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
 
-    negativos = (datos[ (datos['RESULTADO'] == 2) ]
-              .groupby('FECHA_INGRESO')
+    negativos = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 2) ]
+              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])                 
               .count()['ORIGEN'])
 
     # incluyendo uci
-    hospitalizados = (datos[ (datos['RESULTADO'] == 1) & (datos['TIPO_PACIENTE'] == 2) ]
-              .groupby('FECHA_INGRESO')
+    hospitalizados = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 1) & (datos_abiertos['TIPO_PACIENTE'] == 2) ]
+              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
 
-    uci = (datos[ (datos['RESULTADO'] == 1) & (datos['UCI'] == 1) ]
-              .groupby('FECHA_INGRESO')
+    uci = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 1) & (datos_abiertos['UCI'] == 1) ]
+              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
 
-    ambulatorios = (datos[ (datos['RESULTADO'] == 1) & (datos['TIPO_PACIENTE'] == 1) ]
-              .groupby('FECHA_INGRESO')
+    ambulatorios = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 1) & (datos_abiertos['TIPO_PACIENTE'] == 1) ]
+              .groupby(['ENTIDAD_UM', 'FECHA_INGRESO'])
               .count()['ORIGEN'])
 
-    fallecidos = (datos[ (datos['RESULTADO'] == 1) & (datos['FECHA_DEF'] != '9999-99-99') ]
-              .groupby('FECHA_DEF')
+    fallecidos = (datos_abiertos[ (datos_abiertos['RESULTADO'] == 1) & (datos_abiertos['FECHA_DEF'] != '9999-99-99') ]
+              .groupby(['ENTIDAD_UM', 'FECHA_DEF'])
               .count()['ORIGEN'])
-    fallecidos.index = pd.to_datetime(fallecidos.index)
+    # Convierte las fechas de defunción de str a fecha
+    fallecidos.index.set_levels( pd.to_datetime(fallecidos.index.levels[1]), level=1, inplace=True )
+#     fallecidos.index = pd.to_datetime(fallecidos.index)
 
     df.loc[:,'pruebas_diarias']         = pruebas
     df.loc[:,'confirmados_diarios']     = confirmados
@@ -88,126 +81,28 @@ def dataframe_estado(datos, entidades, estado='QUERÉTARO'):
     df.loc[:,'ambulatorios_diarios']    = ambulatorios
     df.loc[:,'fallecidos_diarios']      = fallecidos
 
-
-    df.index = pd.to_datetime(df.index)
-    idx = pd.date_range(df.index.min(), df.index.max())
-    df = df.reindex(idx, fill_value=0)
-    df.index.name = 'Fecha'
+    # Convierte str a fechas y llena hoyos de fechas con ceros
+    df.index.set_levels( pd.to_datetime(df.index.levels[1]), level=1, inplace=True )
+    idx = pd.date_range(df.index.levels[1].min(), df.index.levels[1].max())
+    df.index.set_levels( idx, level=1, inplace=True )
+    df.index.levels[1].name = 'Fecha'
     df = df.fillna(0)
-
-    df.loc[:,'pruebas_acumuladas'] = df.loc[:,'pruebas_diarias'].cumsum()
-    df.loc[:,'confirmados_acumulados'] = df.loc[:,'confirmados_diarios'].cumsum()
-    df.loc[:,'sospechosos_acumulados'] = df.loc[:,'sospechosos_diarios'].cumsum()
-    df.loc[:,'negativos_acumulados'] = df.loc[:,'negativos_diarios'].cumsum()
-    df.loc[:,'hospitalizados_acumulados'] = df.loc[:,'hospitalizados_diarios'].cumsum()
-    df.loc[:,'uci_acumulados'] = df.loc[:,'uci_diarios'].cumsum()
-    df.loc[:,'ambulatorios_acumulados'] = df.loc[:,'ambulatorios_diarios'].cumsum()
-    df.loc[:,'fallecidos_acumulados']   = df.loc[:,'fallecidos_diarios'].cumsum()
+        
+    # Cambia claves de entidades federativas por sus respectivos nombres  
+    df.index.set_levels( entidades.keys, level=0, inplace=True )
+    
+    # Genera series acumuladas para cada tipo de caso
+    df.loc[:,'pruebas_acumuladas'] = df.loc[:,'pruebas_diarias'].groupby(level=0).cumsum()
+    df.loc[:,'confirmados_acumulados'] = df.loc[:,'confirmados_diarios'].groupby(level=0).cumsum()
+    df.loc[:,'sospechosos_acumulados'] = df.loc[:,'sospechosos_diarios'].groupby(level=0).cumsum()
+    df.loc[:,'negativos_acumulados'] = df.loc[:,'negativos_diarios'].groupby(level=0).cumsum()
+    df.loc[:,'hospitalizados_acumulados'] = df.loc[:,'hospitalizados_diarios'].groupby(level=0).cumsum()
+    df.loc[:,'uci_acumulados'] = df.loc[:,'uci_diarios'].groupby(level=0).cumsum()
+    df.loc[:,'ambulatorios_acumulados'] = df.loc[:,'ambulatorios_diarios'].groupby(level=0).cumsum()
+    df.loc[:,'fallecidos_acumulados']   = df.loc[:,'fallecidos_diarios'].groupby(level=0).cumsum()
 
     return df.astype('int')
 
-    # TODO
-    ### DATOS NACIONAL CON PANEL POR ESTADO ###
-    def dataframe_nacional(datos, entidades):
-        """
-        Genera una tabla con los siguientes datos a nivel `estado`*:
-            - pruebas_diarias
-            - confirmados_diarios
-            - sospechosos_diarios
-            - negativos_diarios
-            - hospitalizados_diarios
-            - uci_diarios
-            - ambulatorios diarios
-            - fallecidos_diarios
-            - pruebas_acumuladas
-            - confirmados_acumulados
-            - sospechosos_acumulados
-            - negativos_acumulados
-            - uci_acumulados
-            - ambulatorios_acumulados
-            - fallecidos_acumulados
-
-        Inputs:
-        - datos: datos abiertos de COVID-19 en México disponibles en [1].
-        - entidades: diccionario {nombre_entidad: clave_entidad}
-        - estado: Default='NACIONAL'. Nombre del estado para realizar la tabla
-
-        Output:
-        - series: Series de tiempo para cada una de las columnas mencionadas arriba.
-
-        [1]: https://www.gob.mx/salud/documentos/datos-abiertos-152127
-
-        *Si `estado = 'NACIONAL'`, genera la tabla agregada de todo México
-        """
-
-
-        df = pd.DataFrame()
-
-
-        if estado != 'NACIONAL':
-            # Toma datos del estado únicamente
-            cve_entidad = entidades[estado]
-            datos = datos[ datos['ENTIDAD_UM'] == cve_entidad ]
-
-        datos['FECHA_INGRESO'] = pd.to_datetime(datos['FECHA_INGRESO'])
-
-        pruebas = (datos
-                  .groupby('FECHA_INGRESO')
-                  .count()['ORIGEN'])
-
-        confirmados = (datos[ (datos['RESULTADO'] == 1) ]
-                  .groupby('FECHA_INGRESO')
-                  .count()['ORIGEN'])
-
-        sospechosos = (datos[ (datos['RESULTADO'] == 3) ]
-                  .groupby('FECHA_INGRESO')
-                  .count()['ORIGEN'])
-
-        negativos = (datos[ (datos['RESULTADO'] == 2) ]
-                  .groupby('FECHA_INGRESO')
-                  .count()['ORIGEN'])
-
-        # incluyendo uci
-        hospitalizados = (datos[ (datos['RESULTADO'] == 1) & (datos['TIPO_PACIENTE'] == 2) ]
-                  .groupby('FECHA_INGRESO')
-                  .count()['ORIGEN'])
-
-        uci = (datos[ (datos['RESULTADO'] == 1) & (datos['UCI'] == 1) ]
-                  .groupby('FECHA_INGRESO')
-                  .count()['ORIGEN'])
-
-        ambulatorios = (datos[ (datos['RESULTADO'] == 1) & (datos['TIPO_PACIENTE'] == 1) ]
-                  .groupby('FECHA_INGRESO')
-                  .count()['ORIGEN'])
-
-        fallecidos = (datos[ (datos['RESULTADO'] == 1) & (datos['FECHA_DEF'] != '9999-99-99') ]
-                  .groupby('FECHA_DEF')
-                  .count()['ORIGEN'])
-        fallecidos.index = pd.to_datetime(fallecidos.index)
-
-        df.loc[:,'pruebas_diarias']         = pruebas
-        df.loc[:,'confirmados_diarios']     = confirmados
-        df.loc[:,'sospechosos_diarios']     = sospechosos
-        df.loc[:,'negativos_diarios']       = negativos
-        df.loc[:,'hospitalizados_diarios']  = hospitalizados
-        df.loc[:,'uci_diarios']             = uci
-        df.loc[:,'ambulatorios_diarios']    = ambulatorios
-        df.loc[:,'fallecidos_diarios']      = fallecidos
-
-
-        df.index = pd.to_datetime(df.index)
-        idx = pd.date_range(df.index.min(), df.index.max())
-        df = df.reindex(idx, fill_value=0)
-        df.index.name = 'Fecha'
-        df = df.fillna(0)
-
-        df.loc[:,'pruebas_acumuladas'] = df.loc[:,'pruebas_diarias'].cumsum()
-        df.loc[:,'confirmados_acumulados'] = df.loc[:,'confirmados_diarios'].cumsum()
-        df.loc[:,'sospechosos_acumulados'] = df.loc[:,'sospechosos_diarios'].cumsum()
-        df.loc[:,'negativos_acumulados'] = df.loc[:,'negativos_diarios'].cumsum()
-        df.loc[:,'hospitalizados_acumulados'] = df.loc[:,'hospitalizados_diarios'].cumsum()
-        df.loc[:,'uci_acumulados'] = df.loc[:,'uci_diarios'].cumsum()
-        df.loc[:,'ambulatorios_acumulados'] = df.loc[:,'ambulatorios_diarios'].cumsum()
-        df.loc[:,'fallecidos_acumulados']   = df.loc[:,'fallecidos_diarios'].cumsum()
-
-        return df.astype('int')
+## HELPER FUNCTIONS 
+def get_serie_nacional(series_panel): return series.groupby(level=1).sum()
+def get_serie_estatal(series_panel, estado): return series.loc[estado]
