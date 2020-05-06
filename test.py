8e71ddeb9f3a21672043f5b1e46c4c76f3a5d680
Blas Kolic <blas.kolic@ouce.ox.ac.uk>
2020-05-06 17:05:55 +0100

    Simplifiqué modelo para caso sin acoplamiento de movilidad. Actualicé los tests respectivamente.

--- test.py
+++ test.py
@@ -7,60 +7,35 @@ from arenas_model import *
 
 
 def main():
-    # Set model dimensions. The model has NC compartiments, NP patches and NG age stratas
-    NP = 10
-    NG = 3
+    # Set number of compartiments (S,E,A,I,H,R,D -> 7)
     NC = 7
 
     ##--SET ARTIFICIAL POPULATIONS, AREAS, AND MOBILITIES--##
 
-    # Artificial population distribution
+    # Artificial population
     N = 100_000
-    n_ig = np.random.rand( NP, NG ) * N # number of people
-
-    # Artificial area of regions
-    s_i = np.random.rand(NP) * 1000 # km^2
-
-    # Artificial column stochastic mobility matrix
-    R_ij = np.random.rand( NP,NP )
-    R_ij = R_ij / R_ij.sum(axis=0)
-
 
     ##--SET MODEL PARAMETERS--##
 
     # Arenas parameters
     import arenas_params as ap
     params = [
-        ap.β, ap.kg, ap.η, ap.αg, ap.ν, ap.μg, ap.γg, ap.ωg, ap.ψg, ap.χg, n_ig, R_ij, ap.Cgh, ap.ξ, ap.pg, ap.σ, ap.κ0, ap.ϕ, ap.tc, ap.tf
+        ap.β, ap.kg, ap.η, ap.αg, ap.ν, ap.μg, ap.γg, ap.ωg, ap.ψg, ap.χg, N, ap.σ, ap.κ0, ap.ϕ, ap.tc, ap.tf
     ]
 
     ## Containtment parameters
-    tc = 15
+    tc = 10
     params[-2] = tc
     tf = 10
     params[-1] = tf
-
-    ##--PRECOMPUTE ADDITIONAL PARAMETERS--##
-
-    from ext_params import get_ext_params
-
-    # get some necessary parameters for computation
-    pg = params[14]
-    ξ  = params[13]
-    kg = params[1]
-    one_minus_pg = 1 - pg # recurrent term
-
-    ## Set external fixed parameters
-    ext_params = get_ext_params( n_ig, s_i, R_ij, pg, one_minus_pg, ξ, kg )
-
 
     ##--SET ARTIFICIAL INITIAL CONDITIONS--##
     # I'll put a small amount of asymptomatic cases. The rest will be all susceptible.
 
     # fraction of asymptomatics
     A_frac = 0.01
-    # initial conditions (NCxNPxNG matrix)
-    x0 = np.zeros( [NC, NP, NG] )
+    # initial conditions (NC vector)
+    x0 = np.zeros( NC )
     x0[0] = 1 - A_frac # susceptibles
     x0[2] = A_frac # asymptomatics
 
@@ -68,50 +43,24 @@ def main():
     ###--RUN MODEL--###
     ###-------------###
 
-    # Set number of markovian steps
+    # Set number of days for simulation
     T = 40
     # Obtain model's flow
-    flow = iterate_model(x0, T, params, ext_params)
+    flow = iterate_model(x0, T, params)
+    # Transform densities to absolute population
+    flow = flow * N
 
     ###--SOME PLOTS--###
 
-    # 1.- Plot specific patch for specific age strata (still missing its respective population)
-    i = 0 # patch
-    g = 1 # age strata
+    # Plot the aggregate dynamics
 
-    S_ig =  [flow[t][0][i][g] for t in range(flow.shape[0])]
-    E_ig =  [flow[t][1][i][g] for t in range(flow.shape[0])]
-    A_ig =  [flow[t][2][i][g] for t in range(flow.shape[0])]
-    I_ig =  [flow[t][3][i][g] for t in range(flow.shape[0])]
-    H_ig =  [flow[t][4][i][g] for t in range(flow.shape[0])]
-    R_ig =  [flow[t][5][i][g] for t in range(flow.shape[0])]
-    D_ig =  [flow[t][6][i][g] for t in range(flow.shape[0])]
-
-    plt.plot(S_ig, label='S')
-    plt.plot(E_ig, label='E')
-    plt.plot(A_ig, label='A')
-    plt.plot(I_ig, label='I')
-    plt.plot(H_ig, label='H')
-    plt.plot(R_ig, label='R')
-    plt.plot(D_ig, label='D')
-
-    # containment dates
-    plt.axvline([tc], label='containtment', c='black', lw=1)
-    plt.axvline([tc+tf], label='release', c='black', lw=1, ls='--')
-
-    plt.title('Individual city and age strata')
-    plt.legend()
-    plt.show()
-
-    # Plot the aggregate dynamics (still missing their respective populations; this is just a sum of densities)
-
-    S =  [flow[t][0].sum() for t in range(flow.shape[0])]
-    E =  [flow[t][1].sum() for t in range(flow.shape[0])]
-    A =  [flow[t][2].sum() for t in range(flow.shape[0])]
-    I =  [flow[t][3].sum() for t in range(flow.shape[0])]
-    H =  [flow[t][4].sum() for t in range(flow.shape[0])]
-    R =  [flow[t][5].sum() for t in range(flow.shape[0])]
-    D =  [flow[t][6].sum() for t in range(flow.shape[0])]
+    S =  [flow[t][0] for t in range(flow.shape[0])]
+    E =  [flow[t][1] for t in range(flow.shape[0])]
+    A =  [flow[t][2] for t in range(flow.shape[0])]
+    I =  [flow[t][3] for t in range(flow.shape[0])]
+    H =  [flow[t][4] for t in range(flow.shape[0])]
+    R =  [flow[t][5] for t in range(flow.shape[0])]
+    D =  [flow[t][6] for t in range(flow.shape[0])]
 
     plt.plot(S, label='S')
     plt.plot(E, label='E')
@@ -132,4 +81,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
