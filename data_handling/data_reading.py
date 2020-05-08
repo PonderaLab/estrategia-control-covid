65feac4c8a289ae5f49e13ab3f3efdd6e8fdb98d
Blas Kolic <blas.kolic@ouce.ox.ac.uk>
2020-05-08 15:49:39 +0100

    Metí en una función la lectura de datos abiertos y documenté módulo.

--- data_handling/data_reading.py
+++ data_handling/data_reading.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
-if __name__ == '__main__':
-    ## Hay que automatizar esta variable para que descargue los datos más actuales
-    datos_abiertos_fecha = '20200505' # YYYYMMDD
+'''
+    Este módulo lee los datos abiertos de la DGE [1]. El formato de fecha es YYYYMMDD
+    [1]: https://www.gob.mx/salud/documentos/datos-abiertos-152127
+'''
 
+## Hay que automatizar el argumento de main que descargue los datos más actuales
+def main(datos_abiertos_fecha='20200507'):
     # Path de nuestro repo Mexicovid19 para leer los datos abiertos
     datos_abiertos_path = 'https://raw.githubusercontent.com/mexicovid19/Mexico-datos/master/datos_abiertos/raw/datos_abiertos_{}.csv'.format(datos_abiertos_fecha)
 
     # Lee base de datos
     datos_abiertos = pd.read_csv( datos_abiertos_path )
+    return datos_abiertos
+
+if __name__ == '__main__':
+    main()
