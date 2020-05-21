81d1b68ac9c8d3ae0bff6548c92c5c0383171fc9
fernando <fenandos@hotmail.com>
2020-05-20 19:18:37 -0500

    Read zip file

--- data_handling/data_reading.py
+++ data_handling/data_reading.py
@@ -8,7 +8,7 @@ import pandas as pd
 ## Hay que automatizar el argumento de main que descargue los datos más actuales
 def main(datos_abiertos_fecha='20200509'):
     # Path de nuestro repo Mexicovid19 para leer los datos abiertos
-    datos_abiertos_path = 'https://raw.githubusercontent.com/mexicovid19/Mexico-datos/master/datos_abiertos/raw/datos_abiertos_{}.csv'.format(datos_abiertos_fecha)
+    datos_abiertos_path = 'https://raw.githubusercontent.com/mexicovid19/Mexico-datos/master/datos_abiertos/raw/datos_abiertos_{}.zip'.format(datos_abiertos_fecha)
 
     # Lee base de datos
     datos_abiertos = pd.read_csv( datos_abiertos_path )
